90223095c1c4a56e540fa9d2136fb57c3bdcc134
hong <gmm117@naver.com>
2020-04-12 22:42:06 +0900

    react-thunk add

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -5,13 +5,13 @@ export const increase = () => ({ type : INCREASE });
 export const decrease = () => ({ type : DECREASE });
 
 export const increaseAsync = () => dispatch => {
-    setTimeout(() =? {
+    setTimeout(() => {
         dispatch(increase());
     }, 1000);
 };
 
 export const decreaseAsync = () => dispatch => {
-    setTimeout(() =? {
+    setTimeout(() => {
         dispatch(decrease());
     }, 1000);
 };
