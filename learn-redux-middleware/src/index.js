c1869db8c0eebeddd8e1e6ad9440bab7ac1b1946
hong <gmm117@naver.com>
2020-04-12 22:39:53 +0900

    redux-thunk add

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -12,7 +12,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 
 const store = createStore(rootReducer, 
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 );
 
 ReactDOM.render(
